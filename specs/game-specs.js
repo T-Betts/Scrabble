bf4d252515efad60fced5e354bf7b85e5a6af799
Tom Betts <tom.betts@live.co.uk>
2018-11-08 23:01:15 +0000

    Update game-specs so that they are tested in isolation.

--- specs/game-specs.js
+++ specs/game-specs.js
@@ -1,24 +1,30 @@
-const Game = require('../src/game.js');
 const expect = require('chai').expect;
 const sinon = require('sinon');
+const Game = require('../src/game.js');
+const Player = require('../src/player.js');
 
 describe('Game', () => {
   describe('switchTurn', () => {
     let game;
     beforeEach(function() {
-      game = new Game(["A", "B", "C"]);
+      let boardStub = sinon.stub();
+      let tileBagStub = sinon.stub();
+      let createStub = (name, id) => {
+        return sinon.createStubInstance(Player, {getId: id});
+      }
+      game = new Game(["A", "B", "C"], createStub, boardStub, tileBagStub);
     });
 
     it('should switch the turn to the next player', () => {
       game.switchTurn();
-      expect(game.currentTurn).to.deep.equal(game.players[1].id);
+      expect(game.currentTurn).to.deep.equal(game.players[1].getId());
     });
 
     it('should switch turn back to player 1 after a round of turns is complete', () => {
       game.switchTurn();
       game.switchTurn();
       game.switchTurn();
-      expect(game.currentTurn).to.deep.equal(game.players[0].id);
+      expect(game.currentTurn).to.deep.equal(game.players[0].getId());
     });
   });
 });
--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ const Player = require('./player.js');
 const TileBag = require('./tile-bag.js');
 const Board = require('./board.js');
 
-function Game(playerNamesArray, player = Player, board = new Board, letterBag = new TileBag) {
+function Game(playerNamesArray, player = (name, id) => new Player(name, id), board = new Board, letterBag = new TileBag) {
   this.playerNames = playerNamesArray;
   this.playerCount = playerNamesArray.length;
   this.board = board;
@@ -10,7 +10,7 @@ function Game(playerNamesArray, player = Player, board = new Board, letterBag = 
   this.currentTurn = 1;
   this.players = [];
   this.playerNames.forEach((playerName, index) => {
-    this.players.push(new player(playerName, index + 1));
+    this.players.push(player(playerName, index + 1));
   });
 }
 
