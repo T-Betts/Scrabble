b5b5c8c88defbf390b7dcdf8d1505349075a4abf
Tom Betts <tom.betts@live.co.uk>
2018-11-27 21:56:32 +0000

    Add test to check arraysEqual returns false when two given arrays are not equal.

--- specs/game-specs.js
+++ specs/game-specs.js
@@ -476,10 +476,16 @@ describe('Game', () => {
   });
 
   describe('arraysEqual', () => {
-    it('should return true if two arrays are equal', () => {
-      let arr1 = [0, 1]
-      let arr2 = [0, 1]
+    it('should return true if two given arrays are equal', () => {
+      let arr1 = [0, 1];
+      let arr2 = [0, 1];
       expect(game.arraysEqual(arr1, arr2)).to.deep.equal(true);
+    });
+
+    it('should return false if two given arrays are not equal', () => {
+      let arr1 = [0, 1];
+      let arr2 = [0, '1'];
+      expect(game.arraysEqual(arr1, arr2)).to.deep.equal(false);
     });
   });
 });
